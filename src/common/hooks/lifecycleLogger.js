bc92c3cb7e366f5ff99ed9ddecb48ae92128b993
serii <seriiburduja@gmail.com>
2023-01-13 11:53:16 +0200

    lifecycleLogger

--- src/common/hooks/lifecycleLogger.js
+++ src/common/hooks/lifecycleLogger.js
@@ -1,6 +1,6 @@
 import { onMounted, onUnmounted } from "vue";
 
-export function useLifecycleLogger(payload:) {
+export function useLifecycleLogger(payload) {
   onMounted(() => {
     //eslint-disable-next-line no-console
     console.log(payload.name, "mounted");
