73633058763d63eb7b46d9de0fca80830a0b3e5e
serii <seriiburduja@gmail.com>
2023-01-16 16:44:02 +0200

    info widget start

---
+++ src/components/ui/InfoWidget.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+<template>
+<div class='info-widget'>
+  <div class="info-widget__icon">
+
+  </div>
+  <div class="info-widget__content">
+    <h3 class="info-widget__title">Menu</h3>
+    <p>Filter and sort</p>
+  </div>
+</div>
+</template>
+<style lang="scss" scoped>
+.info-widget {
+
+}
+</style>
