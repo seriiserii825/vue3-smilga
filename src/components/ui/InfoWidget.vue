c3c2336ca6be7f38622f3a58c203654733b429a1
serii <seriiburduja@gmail.com>
2023-01-16 17:53:10 +0200

    some

--- src/components/ui/InfoWidget.vue
+++ src/components/ui/InfoWidget.vue
@@ -1,18 +1,29 @@
 <script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  subtitle: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+})
 </script>
 <template>
-<div class='info-widget'>
-  <div class="info-widget__icon">
-
+  <div class='info-widget'>
+    <div class="info-widget__content">
+      <h3 class="info-widget__title">{{ title }}</h3>
+      <p>{{ subtitle }}</p>
+      <router-link :to="url">More info</router-link>
+    </div>
   </div>
-  <div class="info-widget__content">
-    <h3 class="info-widget__title">Menu</h3>
-    <p>Filter and sort</p>
-  </div>
-</div>
 </template>
 <style lang="scss" scoped>
 .info-widget {
-
 }
 </style>
