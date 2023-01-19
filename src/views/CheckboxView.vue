d7553db86e8edbb2375952db83ae10f0a2671ab2
serii <seriiburduja@gmail.com>
2023-01-19 13:40:47 +0200

    CheckboxView implement functionality

--- src/views/CheckboxView.vue
+++ src/views/CheckboxView.vue
@@ -1,8 +1,23 @@
 <script setup>
 import SectionHeader from "../components/globals/SectionHeader.vue";
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import usePrism from "../hooks/usePrism";
 import PreCode from "../components/ui/PreCode.vue";
+import Checkbox from "../components/ui/Checkbox.vue";
+import CheckboxGroup from "../components/ui/CheckboxGroup.vue";
+
+const checkbox_is_active = ref(false);
+
+
+const fruits = ref([
+  {id: 1, title: "apple"},
+  {id: 2, title: "pear"},
+  {id: 3, title: "nuts"},
+  {id: 4, title: "peach"},
+  {id: 5, title: "banana"},
+]);
+
+const selected_fruits = ref([3]);
 
 const code_js = `
 const checkbox_is_active = ref(true);
@@ -43,11 +58,29 @@ onMounted(() => {
 <template>
   <div class='checkbox-view'>
     <SectionHeader title="Checkbox"/>
+    <div>Checkbox is active: {{ checkbox_is_active }}</div>
+    <br>
+    <Checkbox
+        value="checkbox"
+        label="Checkbox"
+        name="checkbox"
+        id="0"
+        v-model:checked="checkbox_is_active"/>
+    <br><br>
     <PreCode class-name="language-javascript">{{ code_js }}</PreCode>
     <br><br>
     <PreCode>{{ code }}</PreCode>
     <br><br>
     <SectionHeader title="Checkbox Group"/>
+    <br>
+    <div>Selected fruits: {{ selected_fruits }}</div>
+    <br>
+    <CheckboxGroup
+        :options="fruits"
+        name="fruits"
+        v-model:value="selected_fruits"
+    />
+    <br>
     <PreCode class-name="language-javascript">{{ groupe_code_js }}</PreCode>
     <br><br>
     <PreCode>{{ group_code }}</PreCode>
