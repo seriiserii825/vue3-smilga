e67a4b7266476564befc825083d8e05313c52b18
serii <seriiburduja@gmail.com>
2023-05-27 21:39:19 +0300

    color-generator header

--- src/components/color-generator/ColorGeneratorHeader.vue
+++ src/components/color-generator/ColorGeneratorHeader.vue
@@ -3,21 +3,16 @@ import Input from "../ui/Input.vue";
 import Button from "../ui/Button.vue";
 import Values from "values.js"
 import {ref} from "@vue/reactivity";
-
 const props = defineProps({
   color: {
     type: String,
     required: true
   }
 });
-
 const color = ref(props.color);
-
 const error = ref(false);
 const colors = ref([]);
-
 const emits = defineEmits(['emit-colors']);
-
 function generateColors() {
   try {
     colors.value = new Values(color.value).all(10);
@@ -30,21 +25,31 @@ function generateColors() {
 <template>
   <div class='color-generator-header'>
     <h2 class="color-generator-header__title">Color generator</h2>
-    <Input v-model:value="color" :error="error ? `Wrong color, try again` : null"/>
-    <Button label="Generate" color="primary" @click="generateColors"/>
+    <div class="color-generator-header__wrap">
+      <Input v-model:value="color" :error="error ? `Wrong color, try again` : null"/>
+      <div class="color-generator-header__btn">
+        <Button label="Generate" color="primary" @click="generateColors">Generate</Button>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
 .color-generator-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
   margin-bottom: 6rem;
+  &__wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   &__title {
-    margin-right: 1rem;
+    margin-bottom: 3.2rem;
   }
   .input {
     margin-right: 1rem;
+    width: 80%;
+  }
+  &__btn {
+    width: 20%;
   }
 }
 </style>
